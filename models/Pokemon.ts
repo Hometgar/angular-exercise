1ce3309291ed9dabfdd8ceaeb2807572f10ffd23
Mikael <mikeyredon@gmail.com>
2019-05-20 13:58:16 +0200

    add basic stat to pokemon model

--- models/Pokemon.ts
+++ models/Pokemon.ts
@@ -1,7 +1,12 @@
 export class Pokemon {
-    name:string;
 
-    constructor(name:string){
-        this.name = name;
-    }
+    constructor(
+        public name:string,
+        public hp: number,
+        public attack: number,
+        public defense: number,
+        public specialAttack: number,
+        public specialDefense: number,
+        public speed: number
+    ){}
 }
